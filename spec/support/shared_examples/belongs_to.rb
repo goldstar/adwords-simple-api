cad877f466298ab0a0a9c402a6816087804bb243
Robert Graff <robert_graff@yahoo.com>
2018-06-21 14:42:34 -0700

    add belongs_to relation

---
+++ spec/support/shared_examples/belongs_to.rb
@@ -0,0 +1,23 @@
+module AdwordsSimpleApi
+  RSpec.shared_examples "it belongs to" do |klass, singular|
+    describe "##{singular}" do
+      let(:attributes){ attributes_for(singular) }
+      let(:described_class_instance) { described_class.new(described_class_attributes.merge(foreign_key => attributes[:id])) }
+      let(:foreign_key){ "#{singular}_id".to_sym }
+      let(:owner_instance) { klass.new(attributes) }
+      before do
+        allow(klass.service).to receive(:get).with(
+          hash_including(
+            predicates:
+              [{ field: 'Id', operator: 'IN',  values: [described_class_instance.send(foreign_key)] }]
+          )
+        ).and_return(entries: [attributes])
+      end
+
+      it "should get #{singular}" do
+        expect(described_class_instance.send(singular)).to eq(owner_instance)
+      end
+    end
+  end
+
+end
